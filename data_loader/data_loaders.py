47cd05e75d54dfd6620d348d420d38c5e1c7c0c9
julian8897-swansea <78489417+julian8897-swansea@users.noreply.github.com>
2022-09-30 12:04:11 +0100

    update: removed KMNIST

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -18,22 +18,6 @@ class MnistDataLoader(BaseDataLoader):
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
 
 
-class MnistDataLoader(BaseDataLoader):
-    """
-    K-MNIST data loading demo using BaseDataLoader
-    """
-
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
-        trsfm = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ])
-        self.data_dir = data_dir
-        self.dataset = datasets.KMNIST(
-            self.data_dir, train=training, download=True, transform=trsfm)
-        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
-
-
 class CelebDataLoader(BaseDataLoader):
     """
     CelebA data loading
